ecadf430dde214280791c9bb684da5237b310812
ismailify <ismailnuralam@gmail.com>
2022-03-24 11:26:48 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+
+updater = Updater("5207594806:AAGGGcfejx-qN2LFUq_vrzyuOoZj9Py09K8")
